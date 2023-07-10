0c0380be584a31575aefaa315b7a10c454b43e9c
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2023-07-10 12:52:11 +0700

    .

---
+++ lib/page/page0.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'TEST.dart';
+import 'page1.dart';
+import 'page2.dart';
+
+bool isChecked = false;
+
+class Page0 extends StatelessWidget {
+  const Page0({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Page0Body();
+  }
+}
+
+class Page0Body extends StatelessWidget {
+  const Page0Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Page1();
+  }
+}
